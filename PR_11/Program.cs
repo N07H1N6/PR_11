fc18a13a30ba24bdd507ea7d41dcb1bd50c82426
Zer0 <53601161+N07H1N6@users.noreply.github.com>
2022-11-15 11:55:01 +0300

    Add files via upload

---
+++ PR_11/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PR_11
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Здравствтвуйте");
+            Console.WriteLine("Практическая работа № 11");
+            try
+            {
+                int i;
+                string s = null;
+                Console.Write("Введите текст:");
+                s = Console.ReadLine();
+                char[] c = s.ToCharArray();
+                for (i = 0; i <= c.Length; i++) ;
+                {
+                    if (c[i] == c[c.Length - 1]) ;
+
+
+                }
+
+            
+            }
+        } 
+    }
+}
